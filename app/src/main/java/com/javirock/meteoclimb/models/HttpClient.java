7eee90272c645b9c0e46aa772bd7629cb8504c2e
jbarbadillo <javier.barbadillo@gmail.com>
2018-02-28 22:23:02 +0100

    Add http client class

---
+++ app/src/main/java/com/javirock/meteoclimb/models/HttpClient.java
@@ -0,0 +1,18 @@
+package com.javirock.meteoclimb.models;
+
+import okhttp3.OkHttpClient;
+
+
+public class HttpClient {
+    OkHttpClient client = new OkHttpClient();
+
+    public static void get(){
+
+    }
+    public static void post(){
+
+    }
+    public static void put(){
+
+    }
+}
