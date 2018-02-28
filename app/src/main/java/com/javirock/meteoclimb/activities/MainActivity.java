d3070aedd015bc05d32c0db998bd6d9b6f3a1baa
jbarbadillo <javier.barbadillo@gmail.com>
2018-02-28 20:35:01 +0100

    Add first version of code

---
+++ app/src/main/java/com/javirock/meteoclimb/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.javirock.meteoclimb.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.javirock.meteoclimb.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
