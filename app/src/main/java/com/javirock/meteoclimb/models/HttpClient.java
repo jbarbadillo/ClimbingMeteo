961d0c459875fd3cb8fa2eb75f5716bc4a0dc2bc
jbarbadillo <javier.barbadillo@gmail.com>
2018-02-28 22:32:19 +0100

    Add post method

--- app/src/main/java/com/javirock/meteoclimb/models/HttpClient.java
+++ app/src/main/java/com/javirock/meteoclimb/models/HttpClient.java
@@ -1,16 +1,30 @@
 package com.javirock.meteoclimb.models;
 
+import java.io.IOException;
+
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class HttpClient {
-    OkHttpClient client = new OkHttpClient();
-
+    public final static OkHttpClient client = new OkHttpClient(); // TODO: use a builder?
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+    
     public static void get(){
 
     }
-    public static void post(){
-
+    public static String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
     }
     public static void put(){
 
